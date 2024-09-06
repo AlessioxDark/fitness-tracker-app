76618ef18173d11882b07d6b36833a16c69adffa
Alessio40 <alessio40aq@gmail.com>
2024-09-06 16:12:31 +0200

    added homepage

--- Frontend/fitness-tracker-app/src/App.jsx
+++ Frontend/fitness-tracker-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import Homepage from './components/homepage';
-
+import NavBar from './components/NavBar';
 function App() {
 	return (
 		<div>
 			<NavBar></NavBar>
+			<Homepage></Homepage>
 		</div>
 	);
 }
