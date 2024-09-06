c552a49d44d676c810dd564d7f5a4ea1cff33a1a
Alessio40 <alessio40aq@gmail.com>
2024-09-06 16:14:13 +0200

    added hero section

---
+++ Frontend/fitness-tracker-app/src/components/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import hero from '../assets/Hero5.png';
+export default function Homepage() {
+	return (
+		<div className="homepage">
+			<div className="hero-section">
+				<div className="hero-text">
+					<h1 className="hero-title">Sblocca il Tuo Potenziale</h1>
+					<p className="hero-desc">
+						Utilizza la nostra app per raggiungere il fisico che hai sempre
+						sognato
+					</p>
+					<button className="hero-btn">Sbloccalo ora</button>
+				</div>
+				<img src={hero} alt="" className="hero-img" />
+			</div>
+		</div>
+	);
+}
